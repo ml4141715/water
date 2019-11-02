5dba3a88a261beec7b75c83b3c208e9dd6986cca
Weixin Tang <weixtang@cs.stonybrook.edu>
2019-11-02 12:32:37 -0400

    add all baisc dataset

--- cdn/addAll.js
+++ cdn/addAll.js
@@ -24,11 +24,9 @@ class DataSet{
             "react.development.js",
             "react-dom.developpment.js"
         ]
-
         this.__getOneSrciptStr = function ( oneApiStr ) {
             return "<script src=\""+this.prefix+this.api+oneApiStr+"\"></script>";
         }
-
         this._getScriptStr = function ( apiStr ) {
             var total = "";
             for(var i = 0 ; i< apiStr.length ; i++ ){
@@ -36,10 +34,32 @@ class DataSet{
             }
             return total;
         }
-
         this.getScriptStr = function () {
             return this._getScriptStr(this.apiStr);
         }
+
+
+        // now we need all title
+        //<meta charset="UTF-8">
+        //     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        //<title>Bootstrap Theme</title>
+        //<meta http-equiv="content-type" content="text/html;charset=utf-8">
+        //     <meta name="googlebot" content="noindex">
+        //     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+        //    <link rel="icon" href="WeixinLogin/Image/stratego_icon.png">
+
+        this.charType = "<meta http-equiv=\"content-type\" content=\"text/html;charset=utf-8\">";
+        this.blockGoogle = "<meta name=\"googlebot\" content=\"noindex\">";
+        this.fullView = "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, maximum-scale=1.0\">";
+        this.__iconImageName = "leaf.png";
+        this.iconImage = "<link rel=\"icon\" href=\""+this.prefix+this.image+this.__iconImageName+"\">";
+        this.__titleName = "Green Lab";
+        this.titleName = "<title>"+this.__titleName+"</title>";
+        this.getAll= function () {
+            return this.getScriptStr()+this.titleName+this.iconImage+this.fullView+this.blockGoogle+this.charType;
+        }
+
+
 
 
     }
@@ -47,11 +67,18 @@ class DataSet{
 
 }
 
+
+
+
+
 function loadApi() {
 
     var dataSet = new DataSet();
 
-    document.getElementById("weixinApi").innerHTML= dataSet.getScriptStr();
+    var weixinApi =  document.getElementById("weixinApi");
+
+    weixinApi.innerHTML = weixinApi.innerHTML+dataSet.getAll();
+
 
 }
 
