142fdf410c0e37f7435a77e54f725ec42706cba6
Weixin Tang <weixtang@cs.stonybrook.edu>
2019-11-02 12:00:45 -0400

    test1

---
+++ cdn/addAll.js
@@ -0,0 +1,61 @@
+
+
+class DataSet{
+
+
+
+    constructor( prefix ){
+
+        this.prefix = "www.stonybrook.club/cdn/";
+        this.api = "api/";
+        this.image = "image/";
+
+        if(prefix!=null){
+            this.prefix = prefix;
+        }
+
+        this.apiStr = ["all.css",
+            "babel.min.js",
+            "bootstrap.min.css",
+            "bootstrap.min.js",
+            // "jquery.js",
+            "jquery-3.3.1.min.js",
+            "popper.min.js",
+            "react.development.js",
+            "react-dom.developpment.js"
+        ]
+
+        this.__getOneSrciptStr = function ( oneApiStr ) {
+            return "<script src=\""+this.prefix+this.api+"\"></script>";
+        }
+
+        this._getScriptStr = function ( apiStr ) {
+            var total = "";
+            for(var i = 0 ; i< apiStr.length ; i++ ){
+                total = total+ this.__getOneSrciptStr(  apiStr[i] );
+            }
+            return total;
+        }
+
+        this.getScriptStr = function () {
+            return this._getScriptStr(this.apiStr);
+        }
+
+
+    }
+
+
+}
+
+function loadApi() {
+
+    var dataSet = new DataSet();
+
+    document.getElementById("weixinApi").innerHTML= dataSet.getScriptStr();
+
+}
+
+
+
+
+
